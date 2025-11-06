59a9fed2555efbeeec4b4286dcfc852042e17eab
Cruelasss <irula5198@gmail.com>
2025-11-06 16:38:44 +0700

    Buat kerangka (scaffold) untuk layar FormIsian

---
+++ app/src/main/java/com/example/questnavigasiui_249/view/formulir.kt
@@ -0,0 +1,21 @@
+//formulir.kt
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.questnavigasiui_249.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.questnavigasiui_249.R
+
+
+@Composable
+fun FormIsian(
