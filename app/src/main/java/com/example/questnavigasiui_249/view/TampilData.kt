90c546dcbc98dc12f89d4701d7617a5bdf387913
Cruelasss <irula5198@gmail.com>
2025-11-06 16:25:58 +0700

    Tambahkan data statis untuk TampilData

--- app/src/main/java/com/example/questnavigasiui_249/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_249/view/TampilData.kt
@@ -21,3 +21,11 @@ import com.example.questnavigasiui_249.R
 @Composable
 fun TampilData(
 
+    onBackBtnClick: () -> Unit
+) {
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
+        Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
+    )
+
