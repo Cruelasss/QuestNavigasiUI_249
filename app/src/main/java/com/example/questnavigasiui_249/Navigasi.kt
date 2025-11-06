def897fa8474afbe96511093c75ab117923cf092
Cruelasss <irula5198@gmail.com>
2025-11-06 16:23:04 +0700

    Implementasi layar detail dan navigasi kembali

--- app/src/main/java/com/example/questnavigasiui_249/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_249/Navigasi.kt
@@ -1,49 +1,52 @@
 package com.example.questnavigasiui_249
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavHostController
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.padding
+import com.example.questnavigasiui_249.view.FormIsian
+import com.example.questnavigasiui_249.view.TampilData
 
-enum class  Navigasi{
+enum class Navigasi {
     Formulir,
     Detail
 }
 
 @Composable
-
 fun DataApp(
-    navController : NavHostController = rememberNavController(),
-    modifier : Modifier =
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
 ){
-    Scaffold { isiRuang->
+    Scaffold { isiRuang ->
         NavHost(
             navController = navController,
             startDestination = Navigasi.Formulir.name,
-
-            modifier = Modifier.padding(isiRuang)){
-            composable(route = Navigasi.Formulir.name){
-                FormIsian (
-                    //pilihanJK = JenisK.map { id -> context.resources.getString(id) },
-                OnSubmitBtnClick = {
-                    navController.navigate(Navigasi.Detail.name)
-                }
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
+            composable(route = Navigasi.Formulir.name) {
+                FormIsian(
+                    // pilihanJK = JenisK.map { id -> konteks.resources.getString(id) },
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
                 )
+            }
 
-                composable (router = Navigasi.Detail.name){
-                    TampilData(
-                        onBackBtnClick = {cancelAndBackToFormulir(navController)}
-
-                    )
-                }
-
-
+            composable(route = Navigasi.Detail.name) {
+                TampilData(
+                    onBackBtnClick = { cancelAndBackToFormulir(navController) }
                 )
             }
         }
     }
+}
 
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+) {
+    navController.popBackStack(route = Navigasi.Formulir.name, inclusive = false)
+}
