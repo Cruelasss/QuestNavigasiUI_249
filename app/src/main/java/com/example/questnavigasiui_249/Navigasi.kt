fb9cb308805689ceb8783147e62aaeb27f980200
Cruelasss <irula5198@gmail.com>
2025-11-06 09:26:39 +0700

    implementasi rute Formulir dan navigasi ke Detail

--- app/src/main/java/com/example/questnavigasiui_249/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_249/Navigasi.kt
@@ -1,7 +1,13 @@
 package com.example.questnavigasiui_249
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 enum class  Navigasi{
     Formulir,
@@ -17,6 +23,20 @@ fun DataApp(
     Scaffold { isiRuang->
         NavHost(
             navController = navController,
-            startDestination = Navigasi.Formulir
+            startDestination = Navigasi.Formulir.name,
+
+            modifier = Modifier.padding(isiRuang)){
+            composable(route = Navigasi.Formulir.name){
+                FormIsian (
+                    //pilihanJK = JenisK.map { id -> context.resources.getString(id) },
+                OnSubmitBtnClick = {
+                    navController.navigate(Navigasi.Detail.name)
+                }
+                )
+
+
+                )
+            }
+        }
     }
     )
