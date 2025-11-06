fa34ade68763ddb35c5602ad35fdcb85c349fa73
Cruelasss <irula5198@gmail.com>
2025-11-06 16:39:41 +0700

    Implementasi Scaffold dan TopAppBar untuk FormIsian

--- app/src/main/java/com/example/questnavigasiui_249/view/formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_249/view/formulir.kt
@@ -19,3 +19,15 @@ import com.example.questnavigasiui_249.R
 
 @Composable
 fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick: () -> Unit
+) {
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.home),
+                    color = Color.White) },
+                colors = TopAppBarDefaults
+                    .topAppBarColors(containerColor = colorResource(id = R.color.teal_700))
+            )
+        }
